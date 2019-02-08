c0e703c6ed303091707c89a9ca00fb28f442213b
rasheedsuhail <47409891+rasheedsuhail@users.noreply.github.com>
2019-02-08 09:49:36 +0530

    Update number by user.py

--- number by user.py
+++ number by user.py
@@ -2,20 +2,20 @@
 
 # change the values of num1, num2 and num3
 # for a different result
-num1 = 10
-num2 = 14
-num3 = 12
+num3 = 10
+num4 = 14
+num5 = 12
 
 # uncomment following lines to take three numbers from user
 #num1 = float(input("Enter first number: "))
 #num2 = float(input("Enter second number: "))
 #num3 = float(input("Enter third number: "))
 
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-else:
+if (num3>>= num4) and (num3 >= num5):
    largest = num3
+elif (num4 >= num3) and (num4 >= num5):
+   largest = num4
+else:
+   largest = num5
 
-printf("The largest number between",num1,",",num2,"and",num3,"is",largest)
+print("The largest number between",num3,",",num4,"and",num5,"is",largest)
